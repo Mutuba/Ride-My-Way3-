c8c30c2e7ac5db4036a44926d40de62684151242
Mutuba <dmutuba@gmail.com>
2018-07-04 08:57:28 +0300

    Updates run.py

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from app import create_app
 from flask_cors import CORS
 import psycopg2
+from create_tables import create_tables
 
 app = create_app("development")
 CORS(app)
 
 if __name__ == "__main__":
     app.run()
+    create_tables()
+
