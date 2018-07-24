c9f0070b01efeed096550d209be55e59b1acf79f
Mutuba <dmutuba@gmail.com>
2018-07-24 15:27:29 +0300

    Updated app for heroku

--- run.py
+++ run.py
@@ -10,6 +10,5 @@ app = create_app("development")
 CORS(app)
 
 if __name__ == "__main__":
-    app.run()
-    
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
