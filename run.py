5a62672a2227b967aa6d1c64bda19b9a5acae30c
Mutuba <dmutuba@gmail.com>
2018-07-27 10:42:33 +0300

    Updates

--- run.py
+++ run.py
@@ -3,13 +3,11 @@
 from app import create_app
 from flask_cors import CORS
 import os
-import psycopg2
-from create_tables import create_tables
 
 app = create_app("development")
 CORS(app)
 
 if __name__ == "__main__":
-	#port = int(os.environ.get("PORT", 5000))
+	port = int(os.environ.get("PORT", 5000))
     app.run(host='127.0.0.1', port=5000)
 
